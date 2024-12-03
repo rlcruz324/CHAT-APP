d2bd3a4ab5ebf77ff882899e24c5485bf8137a33
RLCruz324 <rlcruz324@csu.fullerton.edu>
2024-12-03 03:14:29 -0800

    2nd part completed ui

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -14,7 +14,7 @@
 // };
 // export default Sidebar;
 
-// STARTER CODE FOR THIS FILE
+//STARTER CODE FOR THIS FILE
 import Conversations from "./Conversations";
 import LogoutButton from "./LogoutButton";
 import SearchInput from "./SearchInput";
@@ -22,10 +22,12 @@ import SearchInput from "./SearchInput";
 const Sidebar = () => {
 	return (
 		<div className='border-r border-slate-500 p-4 flex flex-col'>
-			<SearchInput />
-			<div className='divider px-3'></div>
-			<Conversations />
-			<LogoutButton />
+			<div>
+				<SearchInput />
+					<div className='divider px-3'></div>
+				<Conversations />
+				<LogoutButton />
+			</div>
 		</div>
 	);
 };
